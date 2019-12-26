a65d6770d0917ff1e983e1b53c15d2dcc9b38e80
Miguel Romao <miguelrf95@gmail.com>
2019-12-26 15:16:56 +0000

    prepare for new branch

---
+++ BootlegDiablo/ShortSword.cs
@@ -0,0 +1,19 @@
+﻿namespace BootlegDiablo
+{
+    /// <summary>
+    /// Class shortSword who inherits from weapon
+    /// </summary>
+    public class ShortSword : Weapon
+    {
+        /// <summary>
+        /// Constructor to instantiate with preset values
+        /// </summary>
+        public ShortSword()
+        {
+            MinDamage = 2;
+            MaxDamage = 6;
+            Durability = 24;
+            Name = "Short Sword";
+        }
+    }
+}
---
+++ BootlegDiablo/Weapon.cs
@@ -0,0 +1,28 @@
+﻿namespace BootlegDiablo
+{
+    /// <summary>
+    /// Abstract class weapon to be a template for all the weapons
+    /// </summary>
+    public abstract class Weapon
+    {
+        /// <summary>
+        /// Max Damage of weapon can make
+        /// </summary>
+        public int MaxDamage { get; set; }
+
+        /// <summary>
+        /// Minimum damage weapon can make
+        /// </summary>
+        public int MinDamage { get; set; }
+
+        /// <summary>
+        /// Durability of weapon
+        /// </summary>
+        public int Durability { get; set; }
+
+        /// <summary>
+        /// Name of weapon
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
