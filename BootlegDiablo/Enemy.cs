3916f2e2c6f0d7599202d9ab6590b8447744dec5
Miguel Romao <miguelrf95@gmail.com>
2020-01-03 18:05:54 +0000

    Change enemy attack method to empty, needs rework

--- BootlegDiablo/Enemy.cs
+++ BootlegDiablo/Enemy.cs
@@ -6,23 +6,10 @@ namespace BootlegDiablo
     {
         public int HP { get; set; }
         public int Damage { get; set; }
+
         public virtual void Attack()
         {
-            //// For each enemy in the room try attack
-            //for (int i = 0; i < dr.Enemies.Length; i++)
-            //{
-            //    _enemy = dr.Enemies[i];
 
-            //    // Check adjacent position of enemy
-            //    if (_enemy.Transform.Pos.X == Transform.Pos.X - 1
-            //        || _enemy.Transform.Pos.X == Transform.Pos.X + 1
-            //        || _enemy.Transform.Pos.Y == Transform.Pos.Y + 1
-            //        || _enemy.Transform.Pos.Y == Transform.Pos.Y - 1)
-            //    {
-            //        // Damage to recieve
-            //        _enemy.HP -= Damage;
-            //    }
-            //}
         }
 
         /// <summary>
