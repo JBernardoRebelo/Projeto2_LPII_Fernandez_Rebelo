1903d57081888ca84207ef1c05e12ac4185c4853
Miguel Romao <miguelrf95@gmail.com>
2020-01-05 18:41:54 +0000

    Add XML comments

--- BootlegDiablo/Quitter.cs
+++ BootlegDiablo/Quitter.cs
@@ -3,15 +3,29 @@ using GameEngine;
 
 namespace BootlegDiablo
 {
-    // Simple component which listens for the escape key and terminates the
-    // parent scene
+    /// <summary>
+    /// Simple component which listens for the escape key and terminates the
+    /// parent scene
+    /// </summary>
     public class Quitter : Component
     {
+        /// <summary>
+        /// Variable to make this class an observer of user input
+        /// </summary>
         private KeyObserver keyObserver;
+
+        /// <summary>
+        /// Override to the component Start()
+        /// </summary>
         public override void Start()
         {
             keyObserver = ParentGameObject.GetComponent<KeyObserver>();
         }
+
+        /// <summary>
+        /// Override to component Update(), check if specific observed key
+        /// is pressed
+        /// </summary>
         public override void Update()
         {
             foreach (ConsoleKey key in keyObserver.GetCurrentKeys())
