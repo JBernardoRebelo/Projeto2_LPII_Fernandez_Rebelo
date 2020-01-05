1824c65ab9353ea06256be85be3291b2509675de
Miguel Romao <miguelrf95@gmail.com>
2020-01-05 21:32:57 +0000

    Change AbstractCollider property type and name. Create ObjectCollider class. Change CollisionHandler Update(), and add new members.

--- GameEngine/CollisionHandler.cs
+++ GameEngine/CollisionHandler.cs
@@ -4,53 +4,101 @@ using System.Numerics;
 
 namespace GameEngine
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class CollisionHandler
     {
-        private int xdim, ydim;
+        /// <summary>
+        /// 
+        /// </summary>
+        private int xdim;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        private int ydim;
+
+        /// <summary>
+        /// 
+        /// </summary>
         private readonly GameObject[,] collisionMap;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly GameObject[,] prevCollisionMap;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="xdim"></param>
+        /// <param name="ydim"></param>
         public CollisionHandler(int xdim, int ydim)
         {
             this.xdim = xdim;
             this.ydim = ydim;
             collisionMap = new GameObject[xdim, ydim];
+            prevCollisionMap = new GameObject[xdim, ydim];
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gameObjects"></param>
         public void Update(IEnumerable<GameObject> gameObjects)
         {
+
+            for (int y = 0; y < collisionMap.Length; y++)
+            {
+                for (int x = 0; x < collisionMap.Length; x++)
+                {
+                    prevCollisionMap[x, y] = collisionMap[x, y];
+                }
+            }
+
             Array.Clear(collisionMap, 0, collisionMap.Length);
+
             foreach (GameObject gObj in gameObjects)
             {
                 if (gObj.IsCollidable)
                 {
                     AbstractCollider collider = gObj.GetComponent<
                         AbstractCollider>();
-                    Transform position = gObj.GetComponent<Transform>();
-                    foreach (Vector2 occupied in collider.Occupied)
+
+                    Transform transform = gObj.GetComponent<Transform>();
+
+                    int x = (int)(collider.ColPos.X);
+                    int y = (int)(collider.ColPos.Y);
+
+                    // Throw exception if any of these is out of bounds
+                    if (x < 0 || x >= xdim || y < 0 || y >= ydim)
                     {
-                        int x = (int)(position.Pos.X + occupied.X);
-                        int y = (int)(position.Pos.Y + occupied.Y);
+                        throw new InvalidOperationException(
+                            $"Out of bounds pixel at ({x},{y}) in game"
+                            + $" object '{gObj.Name}'");
+                    }
 
-                        // Throw exception if any of these is out of bounds
-                        if (x < 0 || x >= xdim || y < 0 || y >= ydim)
-                            throw new InvalidOperationException(
-                                $"Out of bounds pixel at ({x},{y}) in game"
-                                + $" object '{gObj.Name}'");
-
-                        // Throw exception if position in collision map already
-                        // contains a game object
-                        if (collisionMap[x, y] != null)
-                            throw new InvalidOperationException(
-                                "Unable to specify coordinate as occupied by "
-                                + $"'{gObj.Name}' since it is previously "
-                                + $"occupied by '{collisionMap[x, y].Name}'");
-
+                    // Throw exception if position in collision map already
+                    // contains a game object
+                    if (collisionMap[x, y] != null)
+                    {
+                        prevCollisionMap.GetValue(collisionMap[x, y].GetHashCode());
+                        throw new InvalidOperationException(
+                            "Unable to specify coordinate as occupied by "
+                            + $"'{gObj.Name}' since it is previously "
+                            + $"occupied by '{collisionMap[x, y].Name}'");
+                    }
+                    else
+                    {
                         // Set coordinate as occupied by the current game object
                         collisionMap[x, y] = gObj;
                     }
+
                 }
             }
+
+            Array.Clear(prevCollisionMap, 0, prevCollisionMap.Length);
 
         }
     }
---
+++ GameEngine/ObjectCollider.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace GameEngine
+{
+    public class ObjectCollider : AbstractCollider
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public override Vector2 ColPos { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Transform _transform;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ObjectCollider() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pos"></param>
+        public ObjectCollider(Vector2 pos)
+        {
+            ColPos = pos;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Start()
+        {
+            _transform = ParentGameObject.GetComponent<Transform>();
+
+            if (_transform == null)
+            {
+                throw new InvalidOperationException(
+                    $"The {nameof(ObjectCollider)} component " +
+                    $"requires a {nameof(Transform)} component");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Update()
+        {
+            ColPos = new Vector2(
+                (int) _transform.Pos.X,
+                (int) _transform.Pos.Y);
+        }
+    }
+}
