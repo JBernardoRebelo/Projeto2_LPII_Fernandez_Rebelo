0b4a090e6a177c4d31b1559eaaf22223bab3255d
JBernardoRebelo <bernasfasttaker@gmail.com>
2020-01-06 14:28:40 +0000

    Start iteration of colliders

--- GameEngine/CollisionHandler.cs
+++ GameEngine/CollisionHandler.cs
@@ -45,7 +45,6 @@ namespace GameEngine
             int x;
             int y;
 
-            AbstractCollider collider;
             Transform transform;
 
             Array.Clear(collisionMap, 0, collisionMap.Length);
@@ -54,50 +53,47 @@ namespace GameEngine
             {
                 if (gObj.IsCollidable)
                 {
-                    collider = gObj.GetComponent<AbstractCollider>();
                     transform = gObj.GetComponent<Transform>();
 
                     // Array de colliders e percorrê-los
-
-                    collider.Colliding = false;
-
-                    x = (int)collider.ColPos.X;
-                    y = (int)collider.ColPos.Y;
-
-                    Vector2 gObjLeft = new Vector2(x - 1, y);
-                    Vector2 gObjRight = new Vector2(x + 1, y);
-                    Vector2 gObjDown = new Vector2(x, y + 1);
-                    Vector2 gObjUp = new Vector2(x, y - 1);
-
-                    if (x == 0 && y == 0)
+                    foreach (AbstractCollider collider in gObj)
                     {
-                        Console.WriteLine(gObj.Name);
-                        //break;
+                        collider.Colliding = false;
+
+                        x = (int)collider.ColPos.X;
+                        y = (int)collider.ColPos.Y;
+
+                        if (x == 0 && y == 0)
+                        {
+                            Console.WriteLine(gObj.Name);
+                            //break;
+                        }
+
+                        // Throw exception if any of these is out of bounds
+                        if (x < 0 || x >= xdim || y < 0 || y >= ydim)
+                        {
+                            throw new InvalidOperationException(
+                                $"Out of bounds pixel at ({x},{y}) in game"
+                                + $" object '{gObj.Name}'");
+                        }
+
+                        if (collisionMap[x, y] != null)
+                        {
+                            collider.Colliding = true;
+
+                            // Warn collider that it is colliding
+                            Console.WriteLine(
+                                "Unable to specify coordinate as occupied by "
+                                + $"'{gObj.Name}' since it is previously "
+                                + $"occupied by '{collisionMap[x, y].Name}'");
+                        }
+                        else
+                        {
+                            // Set coordinate as occupied by the current game object
+                            collisionMap[x, y] = gObj;
+                        }
                     }
 
-                    // Throw exception if any of these is out of bounds
-                    if (x < 0 || x >= xdim || y < 0 || y >= ydim)
-                    {
-                        throw new InvalidOperationException(
-                            $"Out of bounds pixel at ({x},{y}) in game"
-                            + $" object '{gObj.Name}'");
-                    }
-
-                    if(collisionMap[x,y] != null)
-                    {
-                        collider.Colliding = true;
-
-                        // Warn collider that it is colliding
-                        Console.WriteLine(
-                            "Unable to specify coordinate as occupied by "
-                            + $"'{gObj.Name}' since it is previously "
-                            + $"occupied by '{collisionMap[x, y].Name}'");
-                    }
-                    else
-                    {
-                        // Set coordinate as occupied by the current game object
-                        collisionMap[x, y] = gObj;
-                    }
                 }
             }
         }
